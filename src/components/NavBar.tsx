4d5c05d70907c517b837c8da2a1c34744f0f021d
Pol Gubau Amores <gubaupol@gmail.com>
2023-11-07 20:57:27 +0100

    Update app layout and navbar, remove login page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -20,11 +20,11 @@ const NavBar = async () => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <Link href="/" className="flex items-center">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             Auth Template
           </span>
-        </a>
+        </Link>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
